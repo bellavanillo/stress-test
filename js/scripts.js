7f991ebe3a1244ae15755f81c5df661fd849c5c4
bv <bellavanillo@gmail.com>
2018-03-21 20:57:36 -0700

    fix parseInt

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 //Business Logic
 
+var total = (warningSigns + health + activities);
+if (4 <= total <= 8){
+  $("#high-stress").show();
+  $("#mild-stress").hide();
+  $("#low-stress").hide();
+} else if (1 <= total <= 3){
+  $("#mild-stress").show();
+  $("#high-stress").hide();
+  $("#low-stress").hide();
+} else if (-4 <= total <= 0){
+  $("#low-stress").show();
+  $("#mild-stress").hide();
+  $("#high-stress").hide();
+} else {
+  $(".results").hide();
+}
 
 
 
@@ -9,36 +25,21 @@ $(document).ready(function(){
     event.preventDefault();
     debugger;
     //$(".results").show();
-    $("input:checkbox[name=warning]:checked").forEach(function(){
-      var warningSigns = ParseInt($(this).val());
+    $("input:checkbox[name=warning]:checked").each(function(){
+      var warningSigns = parseInt($(this).val());
       //$('.results').append( warningSigns + "<br>");
+      //alert(warningSigns);
     });
 
-    $("input:checkbox[name=health]:checked").forEach(function(){
-      var health = ParseInt($(this).val());
+    $("input:checkbox[name=health]:checked").each(function(){
+      var health = parseInt($(this).val());
       //$('.results').append( health + "<br>");
     });
 
-    $("input:checkbox[name=activities]:checked").forEach(function(){
-      var activities = ParseInt($(this).val());
+    $("input:checkbox[name=activities]:checked").each(function(){
+      var activities = parseInt($(this).val());
       //$('.results').append( activities + "<br>");
     });
     $('#questions').hide();
-    var total = (warningSigns + health + activities);
-    if (4 <= total <= 8){
-      $("#high-stress").show();
-      $("#mild-stress").hide();
-      $("#low-stress").hide();
-    } else if (1 <= total <= 3){
-      $("#mild-stress").show();
-      $("#high-stress").hide();
-      $("#low-stress").hide();
-    } else if (-4 <= total <= 0){
-      $("#low-stress").show();
-      $("#mild-stress").hide();
-      $("#high-stress").hide();
-    } else {
-      $(".results").hide();
-    }
   });
 });
